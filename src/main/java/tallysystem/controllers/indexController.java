e155b2b061b7c1a93499642b676a13f1fe368185
Cheng Lin <cheng.lin@ge.com>
2015-09-17 16:14:14 -0500

    implement getAllCard and setCount mapping

--- src/main/java/tallysystem/controllers/indexController.java
+++ src/main/java/tallysystem/controllers/indexController.java
@@ -1,6 +1,7 @@
 package tallysystem.controllers;
 
-import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,7 +15,7 @@ import tallysystem.models.TallyCard;
 public class indexController {
 	
 	//mock data
-	private HashMap<String,TallyCard> tallyCards= new HashMap<String,TallyCard>();	
+	private Map<String,TallyCard> tallyCards;	
 	
 	//end mock data
 	
@@ -35,21 +36,45 @@ public class indexController {
 		return card.toString();		
 	}
 	
+	@RequestMapping(value="getAllCard",method=RequestMethod.GET)
+	public String getAllCard()
+	{
+		return getAllCard();		
+	}
+	
 	@RequestMapping(value="addCount/{cardName}",method=RequestMethod.GET)
 	public String incrementCardCount(@PathVariable("cardName") String cardName)
 	{
 		TallyCard card=tallyCards.get(cardName);
 		card.addCount();
-		return cardName+"'s count increment by 1, now is : "+card.getCount();
-		
+		return cardName+"'s count increment by 1, now is : "+card.getCount();		
 	}
-
+	
+	@RequestMapping(value="addCount/{cardName}/{count}",method=RequestMethod.GET)
+	public String setCardCount(@PathVariable("cardName") String cardName,
+							   @PathVariable("count") int count)
+	{
+		TallyCard card=tallyCards.get(cardName);
+		card.setCount(count);
+		return cardName+"'s count set to: "+card.getCount();		
+	}
 	
 	
 	
 	
-	//helper method
-	
+	//helper methods
+	public String getAllStringFromMap()
+	{
+		Iterator iterator = this.tallyCards.entrySet().iterator();
+		String text="";
+		while(iterator.hasNext())
+		{
+			Map.Entry pair = (Map.Entry)iterator.next();
+			text+=pair.toString()+"\n";
+			iterator.remove();
+		}
+		return text;
+	}
 	
 
 }
