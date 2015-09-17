babb16296c1d21ba88eac90a62e6772e76bb63ac
Cheng Lin <cheng.lin@ge.com>
2015-09-17 14:26:41 -0500

    initial commit

---
+++ src/test/java/tallysystem/TallySystemApplicationTests.java
@@ -0,0 +1,18 @@
+package tallysystem;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = TallySystemApplication.class)
+@WebAppConfiguration
+public class TallySystemApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
