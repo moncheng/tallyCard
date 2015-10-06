618379f41a023a803fd9409f8189b456b3f7bbc4
Cheng Lin <cheng.lin@ge.com>
2015-10-06 15:10:17 -0500

    change cards to square

--- target/classes/static/js/controller.js
+++ target/classes/static/js/controller.js
@@ -1,33 +1,24 @@
 
 var tallyApp= angular.module('tallyApp',[]);
+var templates =	[  				 
+	{'name':'greenCard',url:'../html/CardGreen.html'},
+	{'name':'lightgreenCard',url:'../html/CardLightGreen.html'},
+	{'name':'medgreenCard',url:'../html/CardMedGreen.html'}
+];
 
-var templates =				[
-            				 
-            				 {'name':'greenCard',url:'../html/CardGreen.html'},
-            				 {'name':'lightgreenCard',url:'../html/CardLightGreen.html'},
-            				 {'name':'medgreenCard',url:'../html/CardMedGreen.html'}
-            				 ];
-
-tallyApp.controller('testController',function($scope)
-		{
-			$scope.templates=;
-	
-		});
+tallyApp.controller('testController', function($scope, TallyService) {
+	$scope.templates=;
+});
 
 
-
-
-
-
-
-function createRandomArray(num)
-{
-	//create a array with size of num or a loop with count of num
-	//set var random range to templates's size
+function createRandomArray(num) {
+	// create a array with size of num or a loop with count of num
+	// set var random range to templates's size
 	// in loop, new array. add old array[random(range)].
- //return new array
+ // return new array
 }
 
 
-//another solutino is, to have a basic Card template, with varies of color them in string or array
-//when assign value to the card from db, assign the color theme too.
+// another solutino is, to have a basic Card template, with varies of color them
+// in string or array
+// when assign value to the card from db, assign the color theme too.
