195d16423297547a1571ecd50b824bba9512844e
Cheng Lin <cheng.lin@ge.com>
2015-10-20 15:50:46 -0500

    create angular directive

--- src/main/resources/static/js/controller.js
+++ src/main/resources/static/js/controller.js
@@ -1,9 +1,31 @@
 
 var tallyApp= angular.module('tallyApp',[]);
 
-tallyApp.controller('testController', function($scope) {
-	$scope.tallyCards=[{'name':'C','count':2},
-	                   {'name':'Java','count':4}];
+tallyApp.controller('CardController', function($scope) {
+	$scope.tallyCards=[{'name':'C','count':2,'id':1},
+	                   {'name':'Java','count':4,'id':2},
+	                   {'name':'Java','count':4,'id':2},
+	                   {'name':'Java','count':4,'id':2},
+	                   {'name':'Java','count':4,'id':2},
+	                   {'name':'Java','count':4,'id':2},
+	                   {'name':'Java','count':4,'id':2},
+	                   {'name':'Java','count':4,'id':2},
+	                   {'name':'Java','count':4,'id':2},
+	                   {'name':'Java','count':4,'id':2},
+	                   {'name':'Java','count':4,'id':2},
+	                   {'name':'Java','count':4,'id':2},
+	                   {'name':'Java','count':4,'id':2},
+	                   {'name':'Java','count':4,'id':2},
+	                   {'name':'Java','count':4,'id':2},
+	                   {'name':'Java','count':4,'id':2},
+	                   {'name':'Java','count':4,'id':2},
+	                   {'name':'Java','count':4,'id':2},
+	                   {'name':'Java','count':4,'id':2},
+	                   {'name':'Java','count':4,'id':2},
+	                   {'name':'Java','count':4,'id':2},
+	                   {'name':'Java','count':4,'id':2},
+	                   {'name':'Java','count':4,'id':2}];
+
 });
 
 
