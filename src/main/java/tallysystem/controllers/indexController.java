46ee4f7348b4c1843cf92f3a75e7c0d156e478bf
Cheng Lin <cheng.lin@ge.com>
2015-10-22 18:13:02 -0500

    loads mock data with .get serive

--- src/main/java/tallysystem/controllers/indexController.java
+++ src/main/java/tallysystem/controllers/indexController.java
@@ -1,5 +1,6 @@
 package tallysystem.controllers;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Iterator;
 
@@ -9,98 +10,85 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import tallysystem.models.TallyCard;
+import tallysystem.services.TallyCardService;
 
 @RestController
-@RequestMapping(value="/")
+@RequestMapping(value = "/")
 public class indexController {
-	
-	//mock data
-	private HashMap<String,TallyCard> tallyCards= new HashMap<String,TallyCard>();	
-	//end mock data
-	
 
-	@RequestMapping(value="newCard/{cardName}",method=RequestMethod.GET)
-	public String createNewCard(@PathVariable("cardName") String cardName)
-	{
-		String returnText="";
-		if(!tallyCards.containsKey(cardName))
-		{
+	TallyCardService cardService = new TallyCardService();
+
+	private HashMap<String, TallyCard> tallyCards = cardService.makeCards();
+	// end mock data
+
+	@RequestMapping(value = "newCard/{cardName}", method = RequestMethod.GET)
+	public String createNewCard(@PathVariable("cardName") String cardName) {
+		String returnText = "";
+		if (!tallyCards.containsKey(cardName)) {
 			TallyCard card = new TallyCard(cardName);
-		    tallyCards.put(cardName,card);
-		    returnText=cardName+" added.";
+			tallyCards.put(cardName, card);
+			returnText = cardName + " added.";
+		} else {
+			returnText = cardName + " already exist, adding count.\n" + addCount(cardName);
 		}
-		else
-		{
-			returnText=cardName+" already exist, adding count.\n"+addCount(cardName);
-		}
-		
-		
+
 		return returnText;
 
 	}
-	@RequestMapping(value="getCard/{cardName}",method=RequestMethod.GET)
-	public TallyCard getCard(@PathVariable("cardName") String cardName)
-	{
-		TallyCard card=tallyCards.get(cardName);
-		
-		return card;		
+
+	@RequestMapping(value = "getCard/{cardName}", method = RequestMethod.GET)
+	public TallyCard getCard(@PathVariable("cardName") String cardName) {
+		TallyCard card = tallyCards.get(cardName);
+
+		return card;
 
 	}
-	
-	@RequestMapping(value="getAllCards",method=RequestMethod.GET)
-	public HashMap<String,TallyCard> getAllCards()
-	{
-		return tallyCards;		
+
+	@RequestMapping(value = "getAllCards", method = RequestMethod.GET)
+	public Collection<TallyCard> getAllCards() {
+
+		return tallyCards.values();
 	}
-	
-	@RequestMapping(value="addCount/{cardName}",method=RequestMethod.GET)
-	public String addCount(@PathVariable("cardName") String cardName)
-	{
-		TallyCard card=tallyCards.get(cardName);
+
+	@RequestMapping(value = "addCount/{cardName}", method = RequestMethod.GET)
+	public String addCount(@PathVariable("cardName") String cardName) {
+		TallyCard card = tallyCards.get(cardName);
 		card.addCount();
-		return cardName+"'s count increment by 1, now is : "+card.getCount();		
+		return cardName + "'s count increment by 1, now is : " + card.getCount();
 	}
-	
-	@RequestMapping(value="addCount/{cardName}/{count}",method=RequestMethod.GET)
-	public String setCardCount(@PathVariable("cardName") String cardName,
-							   @PathVariable("count") int count)
-	{
-		TallyCard card=tallyCards.get(cardName);
+
+	@RequestMapping(value = "addCount/{cardName}/{count}", method = RequestMethod.GET)
+	public String setCardCount(@PathVariable("cardName") String cardName, @PathVariable("count") int count) {
+		TallyCard card = tallyCards.get(cardName);
 		card.setCount(count);
-		return cardName+"'s count set to: "+card.getCount();		
+		return cardName + "'s count set to: " + card.getCount();
 	}
-	
-	@RequestMapping(value="removeCard/{cardName}",method=RequestMethod.GET)
-	public String removeCard(@PathVariable("cardName") String cardName)
-	{
+
+	@RequestMapping(value = "removeCard/{cardName}", method = RequestMethod.GET)
+	public String removeCard(@PathVariable("cardName") String cardName) {
 		tallyCards.remove(cardName);
-		return cardName+" removed.";
-		
+		return cardName + " removed.";
+
 	}
-	
-	@RequestMapping(value="removeAllCards",method=RequestMethod.GET)
-	public String removeAllCards()
-	{
+
+	@RequestMapping(value = "removeAllCards", method = RequestMethod.GET)
+	public String removeAllCards() {
 		tallyCards.clear();
 		return "all cards are removed.";
-		
+
 	}
-	
-	
-	//helper methods
-	public String getAllStringFromMap()
-	{
+
+	// helper methods
+	public String getAllStringFromMap() {
 		Iterator iterator = this.tallyCards.entrySet().iterator();
-		String text="";
-		while(iterator.hasNext())
-		{
-			HashMap.Entry pair = (HashMap.Entry)iterator.next();
-			text+=pair.getValue().toString()+"\n\n";
+		String text = "";
+		while (iterator.hasNext()) {
+			HashMap.Entry pair = (HashMap.Entry) iterator.next();
+			text += pair.getValue().toString() + "\n\n";
 		}
-		if (text=="")
-			text="no cards found.";
+		if (text == "")
+			text = "no cards found.";
 		return text;
 	}
-	
 
 }
--- target/classes/static/js/controller.js
+++ target/classes/static/js/controller.js
@@ -1,31 +1,36 @@
 
 var tallyApp= angular.module('tallyApp',[]);
 
-tallyApp.controller('CardController', function($scope) {
-	$scope.tallyCards=[{'name':'C','count':2,'id':1},
-	                   {'name':'Java','count':4,'id':2},
-	                   {'name':'Java','count':4,'id':2},
-	                   {'name':'Java','count':4,'id':2},
-	                   {'name':'Java','count':4,'id':2},
-	                   {'name':'Java','count':4,'id':2},
-	                   {'name':'Java','count':4,'id':2},
-	                   {'name':'Java','count':4,'id':2},
-	                   {'name':'Java','count':4,'id':2},
-	                   {'name':'Java','count':4,'id':2},
-	                   {'name':'Java','count':4,'id':2},
-	                   {'name':'Java','count':4,'id':2},
-	                   {'name':'Java','count':4,'id':2},
-	                   {'name':'Java','count':4,'id':2},
-	                   {'name':'Java','count':4,'id':2},
-	                   {'name':'Java','count':4,'id':2},
-	                   {'name':'Java','count':4,'id':2},
-	                   {'name':'Java','count':4,'id':2},
-	                   {'name':'Java','count':4,'id':2},
-	                   {'name':'Java','count':4,'id':2},
-	                   {'name':'Java','count':4,'id':2},
-	                   {'name':'Java','count':4,'id':2},
-	                   {'name':'Java','count':4,'id':2}];
-
+tallyApp.controller('CardController', function($scope, $http) {
+	
+	$http.get('http://localhost:8080/getAllCards').then(function(resp) {
+		$scope.tallyCards = resp.data;
+	  })
 });
 
 
+
+
+//[{'name':'C','count':2,'id':1},
+// {'name':'Java','count':4,'id':2},
+// {'name':'Java','count':4,'id':2},
+// {'name':'Java','count':4,'id':2},
+// {'name':'Java','count':4,'id':2},
+// {'name':'Java','count':4,'id':2},
+// {'name':'Java','count':4,'id':2},
+// {'name':'Java','count':4,'id':2},
+// {'name':'Java','count':4,'id':2},
+// {'name':'Java','count':4,'id':2},
+// {'name':'Java','count':4,'id':2},
+// {'name':'Java','count':4,'id':2},
+// {'name':'Java','count':4,'id':2},
+// {'name':'Java','count':4,'id':2},
+// {'name':'Java','count':4,'id':2},
+// {'name':'Java','count':4,'id':2},
+// {'name':'Java','count':4,'id':2},
+// {'name':'Java','count':4,'id':2},
+// {'name':'Java','count':4,'id':2},
+// {'name':'Java','count':4,'id':2},
+// {'name':'Java','count':4,'id':2},
+// {'name':'Java','count':4,'id':2},
+// {'name':'Java','count':4,'id':2}]
