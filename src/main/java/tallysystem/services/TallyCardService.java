a425f4c9b3c41fc91a11ca4d5533684c0012f339
Cheng Lin <cheng.lin@ge.com>
2015-11-03 15:04:54 -0600

    remove mock data

--- src/main/java/tallysystem/services/TallyCardService.java
+++ src/main/java/tallysystem/services/TallyCardService.java
@@ -43,58 +43,9 @@ public class TallyCardService {
 		return  mongoOperations.find(query, TallyCard.class);
 	}
 	public List<TallyCard> getAllCards() {
-		List<TallyCard> cards=mongoOperations.findAll(TallyCard.class);
-		System.out.println("------------------\n"+cards.isEmpty()+"\n----------------------");
-		return cards;
+		return mongoOperations.findAll(TallyCard.class);
+
 	}
 	
-	public HashMap<String, TallyCard> makeCards() {
-		HashMap<String, TallyCard> cards = new HashMap<String, TallyCard>();
-
-		String[] skills = { "Java", "C", "Python", "C++", "C#", "Ruby", "Rails", "AngularJS", "Git", "Javascript",
-				"Html", "CSS", "BootStrap", "Chef", "Swift", "Xcode", "Android", "MS-Office", "Jenkins", "UnitTest",
-				"UI Test", "Database", "Jquery", "NodeJS" };
-		String[] companies = { "GE", "Google", "MicroSoft", "Apple", "Tesla", "Walmart", "Shell", "StarBuck" };
-		String[] typeLoc = { "Home", "Office", "School" };
-		String[] workFor = { "Myself", "Big Company", "School", "Govement" };
-		String[] studyFor = { "MidTerm Exam", "Final Exam", "Class" };// if =
-																		// class,
-																		// append
-																		// skills
-																		// with
-																		// class
-		String[] schools = { "Princeton University", "Harvard University", "Yale University", "Columbia University",
-				"Stanford University", "MIT", "UNO", "LSU" };
-		String[] countries = { "United States", "United Kindom", "China", "Japan", "Korea", "Canada", "Russia",
-				"Mexico", "Span", "Vietnam", "German" };
-		String[] cities = { "New York", "Los Angelas", "San Francisco", "Orlando", "New Orleans", "Chicago", "Bei Jing",
-				"Shang Hai", "Tokyo" };
-		String[] titles = { "Leader", "Restaurant Owner", "Boss", "Manager", "Programmer", "Designer", "CEO", "CIO",
-				"CFO", "COO", "CSO", "Baker", "Chef", "Maker" };
-
-		// all for work
-		cards = addCardsToMap(cards, "Work on ", skills);
-		cards = addCardsToMap(cards, "Work at ", typeLoc);
-		cards = addCardsToMap(cards, "Learn ", skills);
-		cards = addCardsToMap(cards, "Study for ", studyFor);
-		cards = addCardsToMap(cards, "Study at ", schools);
-		cards = addCardsToMap(cards, "Research on ", skills);
-		cards = addCardsToMap(cards, "Live in ", countries);
-		cards = addCardsToMap(cards, "Live in ", cities);
-		cards = addCardsToMap(cards, "Become a ", titles);
-		return cards;
-
-	}
-
-	public HashMap<String, TallyCard> addCardsToMap(HashMap<String, TallyCard> cards, String preText, String[] array) {
-
-		for (String x : array) {
-			String name = preText + x;
-			TallyCard card = new TallyCard();
-			card.setName(name);
-			cards.put(name, card);
-		}
-		return cards;
-	}
 
 }
